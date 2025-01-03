fcc70bae5dac0bf5060f07f3f23705c7efb96c40
surekha-del <surekhasuthar1975@gmail.com>
2025-01-03 11:41:17 +0000

    Start

---
+++ 01neelportfolio/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header/Header";
+import css from './styles/App.module.scss'
+const App = () => {
+  return <div className={`bg-primary ${css.container}`}>
+      <Header/>
+  </div>
+
+}
+
+export default App;
---
+++ 01neelportfolio/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import css from "./Header.module.scss";
+
+const Header = () => {
+    return 
+  <div>mistry</div>
+}
+
+export default Header;
